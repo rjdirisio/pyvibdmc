c50dd65c73ca0771abb938e1dadb6935ed28d64a
rjdirisio <rjdiri@uw.edu>
2020-08-25 15:29:42 -0700

    include matplotlib as a requirement

--- pyvibdmc/analysis/analyzeDMC.py
+++ pyvibdmc/analysis/analyzeDMC.py
@@ -4,7 +4,7 @@ from ..simulation_utilities import *
 class SimInfo():
     def __init__(self,h5Name):
         self.fname = h5Name    
-        self.loadH5()
+        self._loadH5()
         self._mplIinit
     
     def _mplIinit(self):
@@ -12,24 +12,31 @@ class SimInfo():
         'mathtext.fontset': 'dejavusans',
         'font.size':14}
         plt.rcParams.update(params) 
-
-    def loadH5(self):
+        
+    def _loadH5(self):
         with h5py.File(self.fname,'r') as f:
-            print(list(f.keys()))
             self.vrefVsTau = f['vrefVsTau'][:]
             self.popVsTau  = f['popVsTau'][:]
         
+    def get_vref(self):
+        return self.vrefVsTau
+
+    def get_pop(self):
+        return self.popVsTau
+
     def plt_vref_vs_tau(self,photoName="vref_vs_tau.png"):
         plt.plot(self.vrefVsTau[:,0],self.vrefVsTau[:,1],'k')
         plt.xlabel("Time step")
         plt.ylabel(r"Vref ($\mathrm{cm^{-1}}$)")
         plt.savefig(photoName,dpi=300)
+        plt.close()
 
     def plt_pop_vs_tau(self,photoName='pop_vs_tau.png'):
         plt.plot(self.popVsTau[:,0],self.popVsTau[:,1],'k')
         plt.xlabel("Time step")
         plt.ylabel(r"Population or Sum of Weights")
         plt.savefig(photoName,dpi=300)
+        plt.close()
         
     def avg_energy(self,onwards=1000):
         """onwards is an int that tells us where to start averaging (python indexing
