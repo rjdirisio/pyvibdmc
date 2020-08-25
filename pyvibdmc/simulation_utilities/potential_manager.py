2a147b279d090ebae2d693f82b393807145d48b8
rjdirisio <rjdiri@uw.edu>
2020-08-24 18:55:41 -0700

    Fixed potential_manager.py to work correctly with Pool and single calls.

--- pyvibdmc/simulation_utilities/potential_manager.py
+++ pyvibdmc/simulation_utilities/potential_manager.py
@@ -26,34 +26,26 @@ class Potential:
         self.pyFile = python_file
         self.pot_dir = potential_directory
         self.pool = pool
-        self.initialize()
-
-    def initialize(self):
-        if self.pool > 1:
-            #initialize pool
-            self._potPool = mp.Pool(self.pool)
-        else:
-            self._potPool = None
         self._init_pot()
 
     def _init_pot(self):
         import importlib
         """Go to potential directory that houses python function and assign a self._pot variable to it"""
-        cur_dir = os.getcwd()
         os.chdir(self.pot_dir)
         sys.path.insert(0, self.pot_dir)
-        print(os.getcwd())
         module = self.pyFile.split(".")[0]
         x = importlib.import_module(module)
-        print(x)
         self._pot = getattr(x, self.pot_func)
-        print(self._pot)
-        os.chdir(cur_dir)
+        if self.pool > 1:
+            #initialize pool
+            self._potPool = mp.Pool(self.pool)
+        else:
+            self._potPool = None
 
     def getpot(self,cds):
         """Uses the potential function we got to call potential"""
         if self._potPool is not None:
-            cds = np.array_split(self.pool)
+            cds = np.array_split(cds,self.pool)
             res = self._potPool.map(self._pot,cds)
             v = np.concatenate((res))
         else:
@@ -65,7 +57,7 @@ if __name__ == "__main__":
     pot = Potential(potential_function="callPot",
                     python_file="testPot.py",
                     potential_directory=pth,
-                    pool=0)
+                    pool=4)
     cds = np.random.random((1000,7,3))
     vv = pot.getpot(cds)
     print(type(vv))
