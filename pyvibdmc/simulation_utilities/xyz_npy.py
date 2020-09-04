0a36cb2cd04ddcc53af402749986fe005266c6c6
rjdirisio <rjdiri@uw.edu>
2020-09-03 18:53:11 -0700

    Cleaning up documentation

--- pyvibdmc/simulation_utilities/xyz_npy.py
+++ pyvibdmc/simulation_utilities/xyz_npy.py
@@ -2,18 +2,18 @@ import numpy as np
 
 class xyz_npy:
     """Handler of xyz <-> npy conversion, like a personal version of openBabel."""
+
     @staticmethod
     def extractXYZ(fname,atmStr):
-        import pandas as pd
         """
         Extracts the coordinates from an xyz file and returns it as an np array of dimension nxmx3,
         where n = number of geometries, m = number of atoms, and 3 = cartesian coordinates
         :param fname:
-        :type str
         :param atmStr: a list of strings that will be used to parse the file.
-        :type str        
         :return: np.ndarray
         """
+        import pandas as pd
+
         k =  pd.read_table(fname,delim_whitespace=True,names = ['atom','x','y','z'])
         k.dropna(inplace=True)
         someVals = list(set(atmStr))
@@ -27,9 +27,8 @@ class xyz_npy:
     def writeXYZ(xx, fname,atmStrings,cmt=None):
         """
         Writes a numpy array of x,y,z coordinates to a .xyz file
-        :param fname:name of xyz file
-        :type str
-        :param xx:numpy array, either mx3 or nxmx3, where n = number of geometries and m = number of atoms
+        :param fname: name of xyz file
+        :param xx: numpy array, either mx3 or nxmx3, where n = number of geometries and m = number of atoms
         :param atmStrings: list of strings that correspond to the atom type e.g. ["H","H","O"]
         :return: np.ndarray
         """
