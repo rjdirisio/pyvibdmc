0e34aed2686a48ce24dbc6a37e78367f68f01c05
rjdirisio <rjdiri@uw.edu>
2021-05-28 19:20:47 -0700

    Cupy distance matrix as well as bug fixing cupy coulomb.

--- pyvibdmc/simulation_utilities/tensorflow_descriptors/cupy_distance.py
+++ pyvibdmc/simulation_utilities/tensorflow_descriptors/cupy_distance.py
@@ -1,8 +1,7 @@
 import cupy as cp
 import itertools as itt
 
-
-class Coulomb_It:
+class DistIt:
     def __init__(self, zs, sort_mat=False, like_atoms=None):
         self.zs = cp.array(zs)
         self.sort_mat = sort_mat
@@ -43,41 +42,36 @@ class Coulomb_It:
         else:
             return dists
 
-    def sort_coulomb(self, c_mat):
-        num_geoms = c_mat.shape[0]
+    def sort_dist(self, d_mat):
+        num_geoms = d_mat.shape[0]
         """Takes in coulomb matrix and sorts it according to the row norm"""
-        tot_inds = cp.zeros((num_geoms, len(c_mat[0])), dtype=int)
+        tot_inds = cp.zeros((num_geoms, len(d_mat[0])), dtype=int)
         for pairz in self.like_atoms:
             if len(pairz) == 1:
                 tot_inds[:, pairz[0]] = cp.tile(pairz, num_geoms)
             else:
                 pairz = cp.array(pairz)
                 # argsort the pairs
-                norm_mat = cp.argsort(-1 * cp.linalg.norm(c_mat, axis=1))
+                norm_mat = cp.argsort(-1 * cp.linalg.norm(d_mat, axis=1))
                 relevant_idcs = cp.isin(norm_mat, pairz)
                 pair_norm_mat = norm_mat[relevant_idcs].reshape(len(norm_mat), len(pairz))
                 tot_inds[:, pairz] = pair_norm_mat
         # swap the pairs as they were
-        c_mat = c_mat[cp.arange(c_mat.shape[0])[:, None, None], tot_inds[:, None, :], tot_inds[:, :, None]]
-        return c_mat
+        d_mat = d_mat[cp.arange(d_mat.shape[0])[:, None, None], tot_inds[:, None, :], tot_inds[:, :, None]]
+        return d_mat
 
     def run(self, cds):
         """
         Takes in cartesian coordinates, outputs the upper triangle of
         the coulomb matrix.  Option to have it sorted for permutational invariance
         """
-        # get 0.5 * z^0.4
-        rest = cp.ones((len(self.zs), len(self.zs)))
-        cp.fill_diagonal(rest, 0.5 * self.zs ** 0.4)
         # get zii^2/zij matrix
-        zij = cp.outer(self.zs, self.zs)
         # rij
         atm_atm_mat = self.atm_atm_dists(cds, mat=True)
-        coulomb = zij * rest / atm_atm_mat
         # sort each according to norm of rows/columns
         if self.sort_mat:
-            coulomb_s = self.sort_coulomb(coulomb)
+            dist_s = self.sort_dist(atm_atm_mat)
         else:
-            coulomb_s = coulomb
-        upper_coulomb = coulomb_s[:, self.idxs_0, self.idxs_1]
-        return upper_coulomb
+            dist_s = atm_atm_mat
+        upper = dist_s[:, self.idxs_0, self.idxs_1]
+        return upper
