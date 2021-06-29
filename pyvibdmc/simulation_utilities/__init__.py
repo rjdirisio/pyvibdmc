90c2e85c9a8b7126c91e6a5f7b39669d4b855ad0
Ryan DiRisio <35471342+rjdirisio@users.noreply.github.com>
2021-06-29 14:59:12 -0700

    Update __init__.py

--- pyvibdmc/simulation_utilities/__init__.py
+++ pyvibdmc/simulation_utilities/__init__.py
@@ -1,6 +1,6 @@
 from .Constants import *
 from .xyz_npy import *
 from .potential_manager import *
-from .mpi_potential_manager import *
+# from .mpi_potential_manager import * # Don't include in __init__.py since we don't want to require MPI4Py to be installed to run a normal DMC sim
 from .initial_conditioner import *
 from .tensorflow_descriptors import *
