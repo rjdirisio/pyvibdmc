ae444b6950d94df78984376dc78666ddab30c02a
rjdirisio <rjdiri@uw.edu>
2021-08-25 14:11:29 -0700

    Begin imp samp branch

---
+++ pyvibdmc/simulation_utilities/imp_samp_manager.py
@@ -0,0 +1,25 @@
+import numpy as np
+import scipy as sp
+
+
+class TrialWfnManager:
+    """Takes in a function that calls the trial wave function"""
+
+    def __init__(self, trial_function, trial_directory, python_file):
+        pass
+
+    def _init_wfn(self):
+        pass
+
+    def get_psi(self):
+        pass
+
+
+class GuidedDMCManager:
+    """Internal class that:
+       1. Calculates local energy
+       2. Calculates drift terms
+    """
+
+    def __init__(self, t_wfn_manager):
+        pass
