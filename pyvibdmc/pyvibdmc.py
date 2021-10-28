imp samp cont weighting bug fix, now only calc drift twice at first time step.

pyvibdmc/pyvibdmc.py
@@ -302,10 +302,15 @@ class DMC_Sim:
             max_walker = np.argmax(self._cont_wts)
             self._walker_coords[walker] = np.copy(self._walker_coords[max_walker])
             self._walker_pots[walker] = np.copy(self._walker_pots[max_walker])
+
             if self._desc_wt:
                 self._who_from[walker] = self._who_from[max_walker]
             self._cont_wts[max_walker] /= 2.0
             self._cont_wts[walker] = np.copy(self._cont_wts[max_walker])
+            if self.impsamp_manager is not None:
+                self.f_x[walker] = np.copy(self.f_x[max_walker])
+                self.psi_1[walker] = np.copy(self.psi_1[max_walker])
+                self.psi_sec_der[walker] = np.copy(self.psi_sec_der[max_walker])
 
     @property
     def vref_vs_tau(self):
@@ -351,6 +356,10 @@ class DMC_Sim:
             self._walker_coords = self._walker_coords[death_mask]
             self._walker_pots = self._walker_pots[death_mask]
             rand_nums = rand_nums[death_mask]
+            if self.impsamp_manager is not None:
+                self.f_x = self.f_x[death_mask]
+                self.psi_1 = self.psi_1[death_mask]
+                self.psi_sec_der = self.psi_sec_der[death_mask]
             if self._desc_wt:
                 self._who_from = self._who_from[death_mask]
 
@@ -370,6 +379,11 @@ class DMC_Sim:
                                                       self._walker_coords[birth_mask]))
                 self._walker_pots = np.concatenate((self._walker_pots,
                                                     self._walker_pots[birth_mask]))
+                if self.impsamp_manager is not None:
+                    self.f_x = np.concatenate((self.f_x, self.f_x[birth_mask]))
+                    self.psi_1 = np.concatenate((self.psi_1, self.psi_1[birth_mask]))
+                    self.psi_sec_der = np.concatenate((self.psi_sec_der, self.psi_sec_der[birth_mask]))
+
                 if self._desc_wt:
                     self._who_from = np.concatenate((self._who_from,
                                                      self._who_from[birth_mask]))
@@ -413,11 +427,9 @@ class DMC_Sim:
         """
         The random displacement of each of the coordinates of each of the walkers, done in a vectorized fashion. Displaces self._walker_coords
         """
-        if (self.f_x is None or self.psi_1 is None) or self.weighting == 'discrete':
-            # f_x is 2 * dpsi/psi, which is more convenient for the metropolis step
-            # Either first time step of normal sim or every time step of discrete sim
-            # or every tiume step of finite diff imp amp
+        if (self.f_x is None or self.psi_1 is None):
             self.f_x, self.psi_1, self.psi_sec_der = self.impsamp.drift(self._walker_coords)
+
         disps = np.random.normal(0.0,
                                  self._sigmas,
                                  size=np.shape(self._walker_coords.transpose(0, 2, 1))).transpose(0, 2, 1)
@@ -614,7 +626,7 @@ class DMC_Sim:
                     keyz=['coords', 'desc_wts'],
                     valz=[self._parent, self._desc_wts])
                 if self._deb_desc_wt_tracker:
-                    fname=f"{self.output_folder}/wfns/{self.sim_name}_desc_wt_tracker_{prop_step + 1 - self.desc_wt_time_steps}ts.npy"
+                    fname = f"{self.output_folder}/wfns/{self.sim_name}_desc_wt_tracker_{prop_step + 1 - self.desc_wt_time_steps}ts.npy"
                     np.save(fname, np.array(desc_wt_history))
 
             # Explicitly flush log file every 10 time steps, since it gets caught in buffer in HPC systems

pyvibdmc/tests/test_imp_samp.py
@@ -139,16 +139,20 @@ def test_run_dmc_large_ts_morse():
 #                             [0., 0., 0.]]) * 1.01
 #     start_coord = np.expand_dims(water_coord, axis=0)  # Make it (1 x num_atoms x 3)
 #
+#     ex_args = {'dists':[[0,2],[2,1]],
+#                'angs':[[0,2,1]]}
 #     impo = pv.ImpSampManager(trial_function='trial_wavefunction',
 #                              trial_directory=potDir,
-#                              python_file='h2o_trial.py',
-#                              pot_manager=harm_pot)
+#                              python_file='call_trl_h2o.py',
+#                              pot_manager=harm_pot,
+#                              deriv_kwargs=ex_args,
+#                              trial_kwargs=ex_args)
 #
 #     myDMC = pv.DMC_Sim(sim_name="water_impsamp_test",
 #                        output_folder=sim_ex_dir,
 #                        weighting='discrete',
 #                        num_walkers=2000,
-#                        num_timesteps=2000,
+#                        num_timesteps=5000,
 #                        equil_steps=5,
 #                        chkpt_every=10,
 #                        wfn_every=10,